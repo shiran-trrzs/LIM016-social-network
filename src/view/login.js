3f05e2053203813364b91b0609f9413d3128c591
shiran-trrzs <shirannnn.4@gmail.com>
2022-02-08 17:43:31 -0500

    Se añade imágenes para la vista de login.js

--- src/view/login.js
+++ src/view/login.js
@@ -1,27 +1,29 @@
 export default () => {
-  const viewHome = `
+  const viewLogin = `
     <section>
         <h1 class='textLeft'> VIAJA PE </h1>
-        <img></img>
+        <img src='../img/airplane.png'>
     </section>
     <section>
     <div id='welcomeMessage'>
+        <img src='../img/airplane.png'>
         <h2 class='textRight'> Lo que está pasando ahora </h2>
-    <img></img>
     </div>
     <div id='loginContainer'>
-        <input type='email' class='loginBox' id='email' placeholder='Correo electrónico'> </input>
-        <img></img>
-        <input type='password' class='loginBox' id='password' placeholder='Contraseña'></input>
-        <img></img>
+        <img src='../img/mail.png'><input type='email' class='loginBox' id='email' placeholder='Correo electrónico'> </input>
+        
+        <img src='../img/password.png'><input type='password' class='loginBox' id='password' placeholder='Contraseña'></input>
+      
         <a href=''> Olvidé mi contraseña </a>
+
         <button type='submit'> Iniciar sesión </button>
-        <button type='submit'> Continuar con Google </button>
-        <img></img>
+        
+        <img src='../img/google_icon.png'><button type='submit'> Continuar con Google </button>
+        
         <p> ¿No tienes una cuenta? <a href=''> Únete </a> </p>
     </div>
     </section>`;
-  const viewHomeDiv = document.createElement('div');
-  viewHomeDiv.innerHTML = viewHome;
-  return viewHomeDiv;
+  const viewLoginDiv = document.createElement('div');
+  viewLoginDiv.innerHTML = viewLogin;
+  return viewLoginDiv;
 };
