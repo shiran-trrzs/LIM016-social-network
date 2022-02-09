e753c8c6e984327326a7e24cf6b4d69402e18de1
shiran-trrzs <shirannnn.4@gmail.com>
2022-02-08 23:51:27 -0500

    Se crea template para vista de los posteos en el home

---
+++ src/view/post.js
@@ -0,0 +1,26 @@
+export default () => {
+    const viewPost = `
+    <section class="postTimeline">
+        <div>
+            <img class="avatarProfile" src="${urlPhoto}">
+            <div>
+                <h3 class="user-name"> ${userName} </h3>
+                <p class="date-post"> ${datePost} </p>
+            </div>
+        </div>
+
+        <div>
+            <textarea class="content-post"> ${contentPost} </textarea>
+            <div class="iconPost">
+                <!-- <i class="fa-solid fa-circle-heart"></i> -->
+                <i class="fa-brands fa-gratipay"></i>
+                <i class="fa-solid fa-comment-dots"></i>
+                <i class="fa-solid fa-paper-plane"></i>
+            </div>
+        </div>
+    </section>`;
+
+    const viewPostDiv = document.createElement('div');
+    viewPostDiv.innerHTML = viewPost;
+    return viewPostDiv;
+};
