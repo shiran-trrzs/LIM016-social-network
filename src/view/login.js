3b36013470e464ee524150816109670147507ca7
JocelynCamero <jocelyncamero@gmail.com>
2022-02-15 10:09:10 -0500

    Se aplico estilos CSS a la vista del login

--- src/view/login.js
+++ src/view/login.js
@@ -2,29 +2,32 @@ import { signInWithGoogle, signInWithEmail } from '../firebase/firebase-auth.js'
 
 export default () => {
     const viewLogin = `
-    <div class="page">
-        <section id="socialNetwork">
-            <h1 class="textLeft">VIAJA PE</h1>
-            <img class="logo" src="img/airplane.png"/>
-            <h2 class="text textRight">Lo que está pasando ahora</h2>
+        <section id="principalView1">
+            <h1 class="nameSocialNetwork">VIAJA PE</h1>
+            <img class="logo1" src="img/airplane.png"/>    
         </section>
 
-        <section id="formSection">
-            <input type="email" class="loginBox" id="email" placeholder=" &#xf0e0; Correo electronico"/>
-            <input type="password" class="loginBox" id="password" placeholder=" &#xf084; Contraseña" />
-            <a class="text" href=""> Olvidé mi contraseña </a>
-        </section>
+        <section id="principalView2">
+            <div class="hidden">
+                <img class="logo2" src="img/airplane.png"/>
+                <h2 class="text slogan">Lo que está pasando ahora ...</h2>
+            </div>
+            
+            <div id="formSection">
+                <input type="email" class="loginBox" id="email" placeholder=" &#xf0e0; Correo electronico"/>
+                <input type="password" class="loginBox" id="password" placeholder=" &#xf084; Contraseña" />
+                <a class="text" href=""> Olvidé mi contraseña </a>
+            </div>
 
-        <section id="buttons">
-            <button type="submit" class="btn" id="btnLogIn"> Iniciar sesión </button>
-            <button type="submit" class="btn" id="btnGoogle"> 
-            <img class="imgGoogle" src="../img/google_icon.png" />
-            <span>Continuar con Google</span> 
-            </button>
-            <p class="text">¿No tienes una cuenta? <a class="text bold" href="#/registration"> Únete </a> </p>
-        </section>
-
-    </div>`;
+            <div id="buttons">
+                <button type="submit" class="btn" id="btnLogIn"> Iniciar sesión </button>
+                <button type="submit" class="btn" id="btnGoogle"> 
+                    <img class="imgGoogle" src="../img/google_icon.png" />
+                    <span>Continuar con Google</span> 
+                </button>
+                <p class="text">¿No tienes una cuenta? <a class="text bold" href="#/registration"> Únete </a> </p>
+            </div>
+        </section>`;
 
     const viewLoginDiv = document.createElement('div');
     viewLoginDiv.setAttribute('id', 'viewLog');
