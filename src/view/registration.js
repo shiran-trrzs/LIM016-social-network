a4852a5316feb768ef5f36429d1d63b49b912474
JocelynCamero <jocelyncamero@gmail.com>
2022-02-17 18:11:03 -0500

    Se aplico estilos CSS a la vista de registro de usuario

--- src/view/registration.js
+++ src/view/registration.js
@@ -7,25 +7,40 @@ import { addUserInfo } from '../firebase/firebase-data.js';
 
 export default () => {
     const viewRegistration = `
-      <form id = 'formRegistration'>
-          <input type='text' class='registerInput' id='signup-user' placeholder='Nombre de usuario' required> </input>
-          <img></img>
-          <input type='email' class='registerInput' id='signup-email' placeholder='Correo electrónico' required></input>
-          <img></img>
-          <input type='password' class='registerInput' id='signup-password' placeholder='Contraseña' required></input>
-          <img></img>
-          <input type="checkbox" class="checkTerms" id="checkTerms" required </input><label>Acepto los términos 
-          y condiciones de las Polìticas de Privacidad.</label>
-          <button type='submit' id='btnRegister'> Registrarse </button>
-          <img></img>
-          <p> ¿Ya tienes una cuenta? <a href='#/login'> Iniciar sesión </a> </p>
-      
-      </form>`;
+        <section id="principalView1">
+            <h1 class="nameSocialNetwork">VIAJA PE</h1>
+            <img class="logo1" src="img/airplane.png"/>
+        </section>
+
+        <section id="principalView2">
+            <div class="hidden">
+                <img class="logo2" src="img/airplane.png"/>
+                <h2 class="text slogan">Lo que está pasando ahora ...</h2>
+            </div>
+
+            <form id = 'formRegistration'>
+                <input type='text' class='registerInput' id='signup-user' placeholder=' &#xf007;  Nombre de usuario' required> </input>
+                <img></img>
+                <input type='email' class='registerInput' id='signup-email' placeholder=' &#xf0e0;  Correo electrónico' required></input>
+                <img></img>
+                <input type='password' class='registerInput' id='signup-password' placeholder=' &#xf084;  Contraseña' required></input>
+                <img></img>
+                <div id="termsConditions">
+                <input type="checkbox" class="checkTerms" id="checkTerms" required </input>
+                <label class="text">Acepto los términos y condiciones de las Polìticas de Privacidad.</label>
+                </div>
+                <button type='submit' class="btn" id='btnRegister'> Registrarse </button>
+                <img></img>
+                <p class="text"> ¿Ya tienes una cuenta? </p>
+                <a class="text bold" href='#/login'> Iniciar sesión </a> 
+            </form>
+        </section>`;
 
     const viewRegistrationDiv = document.createElement('div');
     viewRegistrationDiv.innerHTML = viewRegistration;
+    viewRegistrationDiv.setAttribute('id', 'viewRegistration');
     const signupForm = viewRegistrationDiv.querySelector('#formRegistration');
-    
+
     signupForm.addEventListener('submit', (e) => {
         e.preventDefault(); // previene que se envíe y borre automaticamente
         const email = viewRegistrationDiv.querySelector('#signup-email').value;
@@ -44,7 +59,7 @@ export default () => {
                         alert('Verifica tu bandeja de entrada para verificar tu cuenta');
                         window.location.hash = '#/';
                         // Email verification sent!
-                        
+
                         addUserInfo(user.uid, newUser, email);
                     });
             })
